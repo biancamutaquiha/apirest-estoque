aebc6aebe00d244a3e76813a25510e9026e5d3dd
Bianca Mutaquiha <biafersil@gmail.com>
2020-04-16 19:14:57 -0300

    eureka client add

--- src/main/java/br/com/mutaquiha/loja/service/LojaService.java
+++ src/main/java/br/com/mutaquiha/loja/service/LojaService.java
@@ -2,6 +2,7 @@ package br.com.mutaquiha.loja.service;
 
 import br.com.mutaquiha.loja.dto.Compra;
 import br.com.mutaquiha.loja.dto.InfoProdutoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,12 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class LojaService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public void save(Compra compra) {
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<InfoProdutoDTO> exchange = restTemplate.exchange("http://localhost:8080/api/produtos/"+compra.getItens().get(0).getId(),
+        ResponseEntity<InfoProdutoDTO> exchange = restTemplate.exchange("http://produto/api/produtos/"+compra.getItens().get(0).getId(),
                 HttpMethod.GET, null, InfoProdutoDTO.class);
 
         System.out.println(exchange.getBody().getNome());
