01e315a7ae29772c65d7adbbdf58bdd25c3ab0eb
Bianca Mutaquiha <biafersil@gmail.com>
2020-04-21 18:06:30 -0300

    added improviment branch

--- src/main/java/br/com/mutaquiha/loja/controller/LojaController.java
+++ src/main/java/br/com/mutaquiha/loja/controller/LojaController.java
@@ -1,8 +1,10 @@
 package br.com.mutaquiha.loja.controller;
 
 import br.com.mutaquiha.loja.dto.Compra;
+import br.com.mutaquiha.loja.dto.InfoProdutoDTO;
 import br.com.mutaquiha.loja.service.LojaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,7 +15,7 @@ public class LojaController {
     LojaService lojaService;
 
     @PostMapping("/compra")
-    public void realizaCompra(@RequestBody Compra compra){
-        lojaService.save(compra);
+    public ResponseEntity<InfoProdutoDTO> realizaCompra(@RequestBody Compra compra){
+        return lojaService.save(compra);
     }
 }
--- src/main/java/br/com/mutaquiha/loja/service/LojaService.java
+++ src/main/java/br/com/mutaquiha/loja/service/LojaService.java
@@ -5,6 +5,7 @@ import br.com.mutaquiha.loja.dto.Compra;
 import br.com.mutaquiha.loja.dto.InfoProdutoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,7 +18,7 @@ public class LojaService {
     @Autowired
     private ProdutoClient produtoClient;
 
-    public void save(Compra compra) {
+    public ResponseEntity<InfoProdutoDTO> save(Compra compra) {
 
         /*ResponseEntity<InfoProdutoDTO> exchange = restTemplate.exchange("http://produto/api/produtos/"+compra.getItens().get(0).getId(),
                 HttpMethod.GET, null, InfoProdutoDTO.class);
@@ -26,6 +27,11 @@ public class LojaService {
 
          */
         InfoProdutoDTO info = produtoClient.findById(compra.getItens().get(0).getId());
-        System.out.println(info.getNome());
+
+        if (info == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
