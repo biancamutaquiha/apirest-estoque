0888583e7badf72deb1519e7b12278b7d267cd74
Bianca Mutaquiha <biafersil@gmail.com>
2020-04-23 20:22:19 -0300

    refactoring

--- src/main/java/br/com/mutaquiha/loja/client/ProdutoClient.java
+++ src/main/java/br/com/mutaquiha/loja/client/ProdutoClient.java
@@ -2,12 +2,15 @@ package br.com.mutaquiha.loja.client;
 
 import br.com.mutaquiha.loja.dto.InfoProdutoDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @FeignClient("produto")
 public interface ProdutoClient {
 
     @RequestMapping("api/produtos/{id}")
-    InfoProdutoDTO findById(@PathVariable long id);
+    Optional<InfoProdutoDTO> findById(@PathVariable long id);
 }
--- src/main/java/br/com/mutaquiha/loja/service/LojaService.java
+++ src/main/java/br/com/mutaquiha/loja/service/LojaService.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
 @Service
 public class LojaService {
 
@@ -26,9 +28,9 @@ public class LojaService {
         System.out.println(exchange.getBody().getNome());
 
          */
-        InfoProdutoDTO info = produtoClient.findById(compra.getItens().get(0).getId());
+        Optional<InfoProdutoDTO> info = produtoClient.findById(compra.getItens().get(0).getId());
 
-        if (info == null){
+        if (info.isEmpty()){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
